837b2ef57bd793055b58e915510be40cc00d36f7
TrevorDouty <tdouty@nnu.edu>
2024-02-20 10:48:16 -0700

    intial project setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,21 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: ['gatsby-plugin-postcss', 'gatsby-plugin-tailwindcss',   {
+    resolve: "gatsby-source-graphql", // <- Configure plugin
+    options: {
+      typeName: "HASURA",
+      fieldName: "hasura", // <- fieldName under which schema will be stitched
+      url: process.env.GATSBY_HASURA_GRAPHQL_URL,
+      refetchInterval: 10 // Refresh every 10 seconds for new data
+    }
+  }],
 }
